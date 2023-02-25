cf7a4d90c6d599cf4196d2f99bd753c7660f8893
pStrachota <p.strachota52@gmail.com>
2023-02-25 21:21:38 +0100

    remove unnecessary fields from user info context

--- context/UserInfoContext.tsx
+++ context/UserInfoContext.tsx
@@ -5,13 +5,8 @@ import {createContext, ReactNode, useContext, useState} from "react";
 interface UserInfoState {
     name: string;
     setName: (name: string) => void;
-    age: string;
-    setAge: (age: string) => void;
     weight: string;
     setWeight: (weight: string) => void;
-    height: string;
-    setHeight: (height: string) => void;
-    // setFood: (food: Food[]) => void;
 }
 
 export const UserInfoContext = createContext<UserInfoState>({
@@ -19,37 +14,23 @@ export const UserInfoContext = createContext<UserInfoState>({
     setName: () => {
 
     },
-    age: "",
-    setAge: () => {
-
-    },
     weight: "",
     setWeight: () => {
 
     },
-    height: "",
-    setHeight: () => {
-
-    }
 });
 
 export const UserInfoProvider = ({children}: { children: ReactNode }) => {
 
     const [name, setName] = useState<string>("");
-    const [age, setAge] = useState<string>("");
     const [weight, setWeight] = useState<string>("");
-    const [height, setHeight] = useState<string>("");
 
     return (
         <UserInfoContext.Provider value={{
             name,
             setName,
-            age,
-            setAge,
             weight,
             setWeight,
-            height,
-            setHeight,
         }}>
             {children}
         </UserInfoContext.Provider>
@@ -64,9 +45,4 @@ export const useUserInfoState = () => {
         throw new Error('useUserInfoState must be used within a UserInfoProvider');
     }
     return context;
-    // const context = useContext(U);
-    // if (context === undefined) {
-    //     throw new Error('useFoodState must be used within a FoodProvider');
-    // }
-    // return context;
 }
