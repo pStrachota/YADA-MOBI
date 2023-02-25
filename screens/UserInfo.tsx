ef2cfab90e26689f22995c98455fe39de14c0d71
pStrachota <p.strachota52@gmail.com>
2023-02-25 21:23:50 +0100

    add name and weight validations

--- screens/UserInfo.tsx
+++ screens/UserInfo.tsx
@@ -3,49 +3,97 @@ import {View} from "react-native";
 import {Button, TextInput} from "react-native-paper";
 import { useUserInfoState } from "../context/UserInfoContext";
 import {useNavigation} from "@react-navigation/native";
-
+import {useForm, Controller} from 'react-hook-form'
+import {HelperText} from "react-native-paper";
 
 const UserInfo: FunctionComponent = () => {
 
-    const {name, setName, weight, setWeight, age, setAge, height, setHeight} = useUserInfoState();
+    const {setName, setWeight} = useUserInfoState();
 
     const navigation = useNavigation();
 
+    const {
+        control,
+        handleSubmit,
+        formState: {errors, isValid}
+    } = useForm({mode: 'onBlur'})
+
+    const onSubmit = (data: { name: string; weight: string; }) => {
+        setName(data.name)
+        setWeight(data.weight)
+        navigation.navigate("Home")
+    }
+
     return (
-        <>
-            <View  >
-
-                <TextInput
-                    label="Name"
-                    value={name}
-                    onChangeText={text => setName(text)}
-                />
-
-                <TextInput
-                    label="Weight"
-                    value={weight}
-                    onChangeText={text => setWeight(text)}
-                />
-
-                <TextInput
-                    label="Age"
-                    value={age}
-                    onChangeText={text => setAge(text)}
-                />
-
-                <TextInput
-                    label="Height"
-                    value={height}
-                    onChangeText={text => setHeight(text)}
-                />
-
-                <Button mode="contained" onPress={() => {
-                    navigation.navigate("Home")
-                }}>
-                    Let's go!
-                </Button>
-            </View>
-        </>
+        <View style={{
+            flex: 1,
+            alignItems: 'center',
+            marginTop: 50,
+        }}>
+            <Controller
+                control={control}
+                name="name"
+                render={({field: {onChange, value, onBlur}}) => (
+                    <TextInput
+                        style={{
+                            marginBottom: 20,
+                            minWidth: 185,
+                        }}
+                        mode={'outlined'}
+                        placeholder="Enter your name here"
+                        value={value}
+                        onBlur={onBlur}
+                        onChangeText={value => onChange(value)}
+                    />
+                )}
+                rules={{
+                    required: {
+                        value: true,
+                        message: 'Name is required!'
+                    },
+                    pattern: {
+                        value: /^[a-zA-Z]+$/,
+                        message: 'Name must be a string!'
+                    }
+                }}
+            />
+            {errors.name && <HelperText style={{
+                marginBottom: 20
+            }} type="error">{errors.name.message}</HelperText>}
+            <Controller
+                control={control}
+                name="weight"
+                render={({field: {onChange, value, onBlur}}) => (
+                    <TextInput
+                        style={{
+                            minWidth: 195,
+                            marginBottom: 20,
+                        }}
+                        mode={'outlined'}
+                        placeholder="Enter your weight here"
+                        value={value}
+                        onBlur={onBlur}
+                        onChangeText={value => onChange(value)}
+                    />
+                )}
+                rules={{
+                    required: {
+                        value: true,
+                        message: 'Weight is required!'
+                    },
+                    pattern: {
+                        value: /^[0-9]+$/,
+                        message: 'Weight must be a positive number!'
+                    }
+                }}
+            />
+            {errors.weight && <HelperText style={{
+                marginBottom: 20
+            }} type="error">{errors.weight.message}</HelperText>}
+            <Button disabled={!isValid} mode="contained" onPress={handleSubmit(onSubmit)}>
+                Let's go!
+            </Button>
+        </View>
     );
 }
 
